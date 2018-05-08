723ee1e4aa5739a8087bb5da0c25d3f2ec69a6f6
dkaps125 <dkapit@umd.edu>
2018-05-08 17:05:39 -0400

    First commit; got server working to submit randomness, store and retrieve requested chunks

---
+++ server.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+
+	"github.com/dkaps125/go-contract/contract"
+)
+
+type FileData struct {
+	Chunks map[string][]byte
+}
+
+type MemStorage struct {
+	Data map[string][]byte
+}
+
+var myAddr string
+var c contract.Contract
+var prevSubs map[uint32]struct{}
+var memory MemStorage
+
+func main() {
+	fmt.Println("Running...")
+
+	if len(os.Args) < 3 {
+		panic("Expected contract address and account parameters")
+	}
+
+	prevSubs = make(map[uint32]struct{})
+	memory.Data = make(map[string][]byte)
+
+	addr := os.Args[1]
+	myAddr = os.Args[2]
+	c, _ = c.Init("contracts/Catalog.json", addr, "http://localhost:9545")
+
+	var b [32]byte
+	copy(b[:], "http://localhost:5437")
+
+	r, _ := c.Transact("chunkServerJoin", myAddr, b)
+	fmt.Printf("%v\n", r)
+
+	go fileListener()
+
+	n, _ := c.RegisterEventListener("SongPublished")
+	c.Listen(n, "SongPublished", fn)
+}
+
+func addAll(req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	var f FileData
+	err := decoder.Decode(&f)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer req.Body.Close()
+
+	for k, v := range f.Chunks {
+		memory.Data[k] = v
+		fmt.Println("Storing chunk with hash " + k)
+	}
+}
+
+func getChunk(hash string) []byte {
+	return memory.Data[hash]
+}
+
+func fileListener() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			w.Write(getChunk(r.URL.Query().Get("hash")))
+			break
+		case "PUT":
+			addAll(r)
+			w.WriteHeader(http.StatusOK)
+			break
+		default:
+			break
+		}
+	})
+
+	http.ListenAndServe(":5437", nil)
+}
+
+func fn(data []interface{}) error {
+	songIndex := data[0].(uint32)
+
+	if _, ok := prevSubs[songIndex]; ok {
+		return nil
+	}
+
+	ran := uint32(rand.Int31())
+
+	c.Transact("chunkServerSubmitRandomness", myAddr, ran, songIndex)
+
+	fmt.Printf("Submitted rand for %d\n", songIndex)
+
+	prevSubs[songIndex] = struct{}{}
+
+	return nil
+}
